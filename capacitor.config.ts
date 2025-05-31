70e887f574493ed7f90348723f93598df55cc39f
AfonsoFernando051 <afonsofernando051@gmail.com>
2025-05-31 10:02:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reconhecimento-voz',
+  webDir: 'www'
+};
+
+export default config;
